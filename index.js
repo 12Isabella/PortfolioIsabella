a72f4ece35cdfbc89e61f634715a33903cdbe2c3
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-11-03 14:57:11 +0100

    start thought process on image organisation with js

---
+++ index.js
@@ -0,0 +1,22 @@
+let folder1 = [
+  <img
+    src="images/memos/arraysElementExistance/1.jpg"
+    class="image img-fluid"
+    alt="img1"
+  />,
+  <img
+    src="images/memos/arraysElementExistance/2.jpg"
+    class="image img-fluid"
+    alt="img2"
+  />,
+  <img
+    src="images/memos/arraysElementExistance/3.jpg"
+    class="image img-fluid"
+    alt="img3"
+  />,
+];
+
+//this could be in an extra folder, then I can try to run the images in each folder through a forEach...
+//is this really better than having all the images hardcoded in the html thogh??
+
+let folderStructureHTML = `<div class="row">`;
