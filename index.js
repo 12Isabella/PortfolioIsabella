2b36fea3fb60867a6f0628fe66bdcfbc32d9635b
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-11-09 13:58:49 +0100

    organise memos in js + forEach

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Hello");
 
 let folder1 = [{ name: "1" }, { name: "2" }, { name: "3" }];
 
-let memonFolder1 = document.querySelector("#memoFolder1");
+let memoFolder1 = document.querySelector("#memoFolder1");
 
 let memoHTML = `<div class="row"><div class="col-md picture-bg">`;
 
@@ -17,5 +17,104 @@ folder1.forEach((image) => {
 
 memoFolder1.innerHTML = memoHTML + `</div>`;
 
-//this could be in an extra folder, then I can try to run the images in each folder through a forEach...
-//is this really better than having all the images hardcoded in the html thogh??
+let folder2 = [{ name: "1" }, { name: "2" }, { name: "3" }, { name: "4" }];
+
+let memoFolder2 = document.querySelector("#memoFolder2");
+
+let memoHTML2 = `<div class="row"><div class="col-md picture-bg">`;
+
+folder2.forEach((image) => {
+  memoHTML2 =
+    memoHTML2 +
+    `<img
+    src="images/memos/thisKeyword/${image.name}.jpg"
+    class="image img-fluid"
+  />`;
+});
+
+memoFolder2.innerHTML = memoHTML2 + `</div>`;
+
+let folder3 = [{ name: "1" }, { name: "2" }, { name: "3" }];
+
+let memoFolder3 = document.querySelector("#memoFolder3");
+
+let memoHTML3 = `<div class="row"><div class="col-md picture-bg">`;
+
+folder3.forEach((image) => {
+  memoHTML3 =
+    memoHTML3 +
+    `<img
+    src="images/memos/objects/${image.name}.jpg"
+    class="image img-fluid"
+  />`;
+});
+
+memoFolder3.innerHTML = memoHTML3 + `</div>`;
+
+let folder4 = [{ name: "1" }, { name: "2" }, { name: "3" }];
+
+let memoFolder4 = document.querySelector("#memoFolder4");
+
+let memoHTML4 = `<div class="row"><div class="col-md picture-bg">`;
+
+folder4.forEach((image) => {
+  memoHTML4 =
+    memoHTML4 +
+    `<img
+    src="images/memos/mediaQueries/${image.name}.jpg"
+    class="image img-fluid"
+  />`;
+});
+
+memoFolder4.innerHTML = memoHTML4 + `</div>`;
+
+let folder5 = [{ name: "1" }, { name: "2" }];
+
+let memoFolder5 = document.querySelector("#memoFolder5");
+
+let memoHTML5 = `<div class="row"><div class="col-md picture-bg">`;
+
+folder5.forEach((image) => {
+  memoHTML5 =
+    memoHTML5 +
+    `<img
+    src="images/memos/responsiveGrids/${image.name}.jpg"
+    class="image img-fluid"
+  />`;
+});
+
+memoFolder5.innerHTML = memoHTML5 + `</div>`;
+
+let folder6 = [{ name: "1" }, { name: "2" }];
+
+let memoFolder6 = document.querySelector("#memoFolder6");
+
+let memoHTML6 = `<div class="row"><div class="col-md picture-bg">`;
+
+folder6.forEach((image) => {
+  memoHTML6 =
+    memoHTML6 +
+    `<img
+    src="images/memos/SEO/${image.name}.jpg"
+    class="image img-fluid"
+  />`;
+});
+
+memoFolder6.innerHTML = memoHTML6 + `</div>`;
+
+let folder7 = [{ name: "1" }, { name: "2" }];
+
+let memoFolder7 = document.querySelector("#memoFolder7");
+
+let memoHTML7 = `<div class="row"><div class="col-md picture-bg">`;
+
+folder7.forEach((image) => {
+  memoHTML7 =
+    memoHTML7 +
+    `<img
+    src="images/memos/staticWebsite/${image.name}.jpg"
+    class="image img-fluid"
+  />`;
+});
+
+memoFolder7.innerHTML = memoHTML7 + `</div>`;
